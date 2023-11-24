b8830a3042972e405da397af2ff99cb3caf0bb01
Aanshu4k <125659281+Aanshu4k@users.noreply.github.com>
2023-11-24 17:15:49 +0530

    Add files via upload

---
+++ Controllers/AddressClass.cs
@@ -0,0 +1,37 @@
+﻿
+namespace ProjectDSK
+{
+    public class AddressClass
+    {
+
+        public string HNo { get; set; }
+        public string Floor { get; set; }
+        public string BuildName { get; set; }
+        public string Street { get; set; }
+        public string Area { get; set; }
+        public string LandmarkDetails { get; set; }
+        public string CityPostalCode { get; set; }
+        public string NearLoc { get; set; }
+        public string Division { get; set; }
+        public string LandmarkIndicate { get; set; }
+    }
+    public class supplyAddressClass
+    {
+
+        public string HNo { get; set; }
+        public string Floor { get; set; }
+        public string BuildName { get; set; }
+        public string Street { get; set; }
+        public string Area { get; set; }
+        public string LandmarkDetails { get; set; }
+        public string CityPostalCode { get; set; }
+        public string NearLoc { get; set; }
+        public string Division { get; set; }
+        public string LandmarkIndicate { get; set; }
+    }
+    public class FloorMst
+    {
+        public int floor_id { get; set; }
+        public string floor { get; set; }
+    }
+}
---
+++ Controllers/LoginPage.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectDSK
+{
+        public class Login
+        {
+            public int mobileNo { get; set; }
+            public string Email { get; set; }
+            public string uuid { get; set; }
+        }
+}
